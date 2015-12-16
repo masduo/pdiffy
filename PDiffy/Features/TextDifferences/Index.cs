12c2fab58b09951e70ba38ee0e0369bc98e1b38a
masoud.azizpour <masoud.azizpour@seatwave.com>
2015-12-16 18:15:49 +0000

    Updated the razor view, also amended stylesheet. Changed the index class to load the values from json store.
    Signed-off-by: masoud.azizpour <masoud.azizpour@seatwave.com>

--- PDiffy/Features/TextDifferences/Index.cs
+++ PDiffy/Features/TextDifferences/Index.cs
@@ -19,23 +19,30 @@ namespace PDiffy.Features.TextDifferences
 
 			public bool ComparisonExists { get; set; }
 			public bool ComparisonStillValid { get; set; }
+
+			public string OriginalText { get; set; }
+			public string ComparisonText { get; set; }
+			public string DifferenceText { get; set; }
 		}
 
 		public class QueryHandler : IAsyncRequestHandler<Query, IList<Model>>
 		{
 			public async Task<IList<Model>> Handle(Query message)
 			{
-				//var pages = Data.Biggy.PageList.Select(page => new Model
-				//{
-				//	Name = page.Name,
-				//	LastComparisonDate = page.LastComparisonDate,
-				//	ComparisonStillValid = page.ComparisonStillValid,
-				//	HumanComparisonRequired = page.HumanComparisonRequired,
-				//	ComparisonExists = !(string.IsNullOrWhiteSpace(page.ComparisonImageUrl) && string.IsNullOrWhiteSpace(page.ComparisonImagePath))
-				//}).ToList();
+				var pages = Data.Biggy.TextDiffPageList.Select(page => new Model
+				{
+					Name = page.Name,
+					LastComparisonDate = page.LastComparisonDate,
+					ComparisonStillValid = page.ComparisonStillValid,
+					HumanComparisonRequired = page.HumanComparisonRequired,
+					ComparisonExists = string.IsNullOrWhiteSpace(page.DifferenceText) == false,
 
-				//return pages;
-				return new List<Model>();
+					OriginalText = page.OriginalText,
+					ComparisonText = page.ComparisonText,
+					DifferenceText = page.DifferenceText,
+				}).ToList();
+
+				return pages;
 			}
 		}
 	}
