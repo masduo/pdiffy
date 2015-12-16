73f348f43af710515ac94329f8c6e68542291f85
masoud.azizpour <masoud.azizpour@seatwave.com>
2015-12-16 18:05:42 +0000

    Added new json store for text comparisons.
    Signed-off-by: masoud.azizpour <masoud.azizpour@seatwave.com>

--- PDiffy/Data/Biggy.cs
+++ PDiffy/Data/Biggy.cs
@@ -1,30 +1,44 @@
-﻿using Biggy.Core;
+﻿using System;
+using Biggy.Core;
 using Biggy.Data.Json;
-using PDiffy.Infrastructure;
+using Environment = PDiffy.Infrastructure.Environment;
 
 namespace PDiffy.Data
 {
 	public class Biggy
 	{
-		private static BiggyList<Page> _biggyPageList;
-	    private static BiggyList<KnownImage> _biggyKnownImageList;
+		//NOTE: If it is needed to clean data, leave an empty json array [{}] in the file or delete it,
+		// otherwise biggy's list initialization will fail with source cannot be null exception.
 
-	    public static BiggyList<Page> PageList
+		private static BiggyList<Page> _biggyPageList;
+		private static BiggyList<KnownImage> _biggyKnownImageList;
+		private static BiggyList<TextDiffPage> _textDiffPageList;
+
+		public static BiggyList<Page> PageList
 		{
 			get
 			{
-				return _biggyPageList 
+				return _biggyPageList
 					?? (_biggyPageList = new BiggyList<Page>(new JsonStore<Page>(Environment.DataStorePath, "Biggy", "Pages")));
 			}
 		}
 
-	    public static BiggyList<KnownImage> KnownImageList
-	    {
-	        get
-	        {
-	            return _biggyKnownImageList
-                    ?? (_biggyKnownImageList = new BiggyList<KnownImage>(new JsonStore<KnownImage>(Environment.DataStorePath, "Biggy","KnownImages")));
-	        }
-	    }
+		public static BiggyList<KnownImage> KnownImageList
+		{
+			get
+			{
+				return _biggyKnownImageList
+					?? (_biggyKnownImageList = new BiggyList<KnownImage>(new JsonStore<KnownImage>(Environment.DataStorePath, "Biggy", "KnownImages")));
+			}
+		}
+
+		public static BiggyList<TextDiffPage> TextDiffPageList
+		{
+			get
+			{
+				return _textDiffPageList
+						?? (_textDiffPageList = new BiggyList<TextDiffPage>(new JsonStore<TextDiffPage>(Environment.DataStorePath, "Biggy", "TextDiffPages")));
+			}
+		}
 	}
 }
