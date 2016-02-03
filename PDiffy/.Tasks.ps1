b6ff0957c84624eb3f21079e40c401a7ab114ea3
Masoud Azizpour <masoud.azizpour@seatwave.com>
2016-02-03 18:34:19 +0000

    Added OctoPack plugin.

--- PDiffy/.Tasks.ps1
+++ PDiffy/.Tasks.ps1
@@ -3,3 +3,5 @@ Include-PluginScripts
 task Test NUnit, MSpec #, etc. >> enables composition of running all tests of different types under one task
 
 task dev Clean, Compile, Test
+
+task ci Clean, Compile, Test, OctoPack
