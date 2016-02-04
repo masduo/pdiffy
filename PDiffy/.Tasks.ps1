bdc90e2e8a80a1d6bdf6763e28045e7d556e15fe
Masoud Azizpour <masoud.azizpour@seatwave.com>
2016-02-04 13:20:51 +0000

    Added PowerTasks plugins Version, OctoPack, and OctoCreateRelease. Also updated AssemblyInfo.

--- PDiffy/.Tasks.ps1
+++ PDiffy/.Tasks.ps1
@@ -1,7 +1,18 @@
 Include-PluginScripts
 
-task Test NUnit, MSpec #, etc. >> enables composition of running all tests of different types under one task
+#bundle all test tasks here
+task Test NUnit, MSpec
 
-task dev Clean, Compile, Test
+#composes the basic tasks
+task basics Clean, Compile, Test
 
-task ci Clean, Compile, Test, OctoPack
+#dev should run locally, providing the variables are set
+task dev variables, basics
+
+task variables
+{
+
+}
+
+#ci should run on build servers
+task ci basics, Version, OctoPack, Push, OctoCreateRelease
